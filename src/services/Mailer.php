d75b8edb1c8dd2145c10ed0021172cd795947ee6
Lucia Badea <badealucia88@yahoo.com>
2021-07-31 15:56:49 +0200

    Create Form Contact with PHPMailer procedural #5

--- src/services/Mailer.php
+++ src/services/Mailer.php
@@ -1,59 +1,68 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-namespace App\src\services;
-
-use \Mailjet\Resources;
-
-  class Mailer
-  {
-      public function send()
-      {
-
-  define('API_USER', '126a7f0de3710b04e7ddc1a53eee4505');
-  define('API_LOGIN', 'fcdbd2ec2e0e393c7428b7f01a41d2b3');
-  $mj = new \Mailjet\Client(API_USER,API_LOGIN,true,['version' => 'v3.1']);
-
-
-    if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['object']) && !empty($_POST['message'])){
-        $firstName = htmlspecialchars($_POST['name']);
-        $email = htmlspecialchars($_POST['email']);
-        $objet = htmlspecialchars($_POST['object']);
-        $message = htmlspecialchars($_POST['message']);
-
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-            $body = [
-                'Messages' => [
-                [
-                    'From' => [
-                    'Email' => "humulesculucia88@gmail.com",
-                    'Name' => "Lucia"
-                    ],
-                    'To' => [
-                    [
-                        'Email' => "humulesculucia88@gmail.com",
-                        'Name' => "Lucia"
-                    ]
-                    ],
-                    'Subject' => "Demande de renseignement",
-                    'TextPart' => "$email, $message",
-                    'CustomID' => "AppGettingStartedTest"
-                ]
-                ]
-            ];
-            $response = $mj->post(Resources::$Email, ['body' => $body]);
-            $response->success();
-            echo "Email envoyé avec succés !";
-    
-        }else{
-            echo "Email non valide";
-        }
-    } 
-    else{
-        header('Location: ../public/index.php?route=contactForm');
-        die();
-    }
+if(empty($_POST['name']))
+{
+	header('Location:contact_Form.php?error=name');
 }
+if(empty($_POST['email']))
+{
+	header('Location:contact_Form.php?error=email');
 }
-   
+if(empty($_POST['object']))
+{
+	header('Location:contact_Form.php?error=object');
+}
+if(empty($_POST['message']))
+{
+	header('Location:contact_Form.php?error=message');
+}
+
+require 'vendor/autoload.php';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$object = $_POST['object'];
+$message = $_POST['message'];
 
 
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'humulesculucia88@gmail.com';                     //SMTP username
+    $mail->Password   = 'maisonjaune02112017';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = 587;  
+                                      //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('badealucia88@yahoo.com', 'Lucia');
+    $mail->addAddress('humulesculucia88@gmail.com');     //Add a recipient
+
+    $content = "
+
+    Cher Administrateur,
+
+    {$name} t'as contacté par ton site web.
+    L'objet d'email est : {$object} et son email : est {$email}.
+    Son message est : {$message}. "; 
+
+    //Content
+    $mail->isHTML(true);      
+    $mail->Subject = $object;
+    $mail->Body    = $content;
+    $mail->AltBody = strip_tags($content);
+
+    $mail->send();
+    echo 'Votre message a été envoyé';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
